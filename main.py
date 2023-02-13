f3e39604827180d4302902c32ce0dca5ab904b9b
Samuel Wehrli <samuel.wehrli@gmail.com>
2023-02-13 14:02:05 +0100

    Added more advice and a selection for the problem

--- main.py
+++ main.py
@@ -4,10 +4,29 @@ import streamlit as st
 import pandas as pd
 from random import choice
 
-# load data
-#df = pd.read_csv('data.csv')
+# title
+st.title('Ratschläge für betreuende Angehörige')
 
-# read text file advice.txt and store all data in variable text.
+problem_list = [
+    'Kommunikation',
+    'Essen und Trinken',
+    'Körperpflege',
+    'Mobilität',
+    'Ernährung',
+    'Finanzen',
+    'Psychische Gesundheit',
+    'Soziale Kontakte',
+    'Sonstiges']
+
+# dropdown to select problem
+problem = st.selectbox('Welches Problem betrifft dich?', problem_list)
+if problem == 'Essen und Trinken':
+    st.info('Für dieses Thema haben wir gute Ratschläge.')
+else:
+    st.warning('Für dieses Thema haben wir leider noch keine Ratschläge. Im Moment gibt es nur Ratschläge für Essen und Trinken.')
+
+
+
 # Use utf-8 encoding to avoid errors.
 with open('advice.txt', 'r', encoding='utf-8') as f:
     text = f.read()
@@ -16,7 +35,7 @@ with open('advice.txt', 'r', encoding='utf-8') as f:
 advice_list = [x.strip() for x in text.split('-') if x.strip()]
 
 # button
-if st.button('Gib mir einen Ratschlag!'):
+if st.button('Gib mir einen Ratschlag!') & (problem == 'Essen und Trinken'):
     # display random row from data
     st.write(choice(advice_list))
 
